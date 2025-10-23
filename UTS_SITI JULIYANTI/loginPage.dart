70d1f263ccf5377420b23e9f5a0d5ba113cd4622
julynazhr16 <sitijuliyantii06@gmail.com>
2025-10-23 19:35:17 +0700

    update

--- UTS_SITI JULIYANTI/loginPage.dart
+++ UTS_SITI JULIYANTI/loginPage.dart
@@ -6,3 +6,21 @@ class UTSLogin extends StatefulWidget {
   @override
   State<UTSLogin> createState() => _UTSLoginState();
 }
+
+class _UTSLoginState extends State<UTSLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "gambar/logo,png",
+            )
+          ],
+        ),
+      ),
+    )
+  }
+}

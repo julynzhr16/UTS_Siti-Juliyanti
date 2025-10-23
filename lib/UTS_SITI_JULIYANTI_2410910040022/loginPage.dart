11caf6c7906ab8d343b831e1df225f1a2bd25b5c
julynazhr16 <sitijuliyantii06@gmail.com>
2025-10-23 21:36:01 +0700

    update

--- lib/UTS_SITI_JULIYANTI_2410910040022/loginPage.dart
+++ lib/UTS_SITI_JULIYANTI_2410910040022/loginPage.dart
@@ -11,28 +11,52 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: ListView(
         children: [
-          Positioned(
-            top: 50,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          Stack(
+            children: [
+              Positioned(
+                top: 50,
+                left: 0,
+                right: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("gambar/logo.png", width: 100),
+                    SizedBox(width: 10),
+                    Text(
+                      "LKS MART",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Container(
+            margin: const EdgeInsets.only(top: 200, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.asset("gambar/logo.png", width: 100),
-                SizedBox(width: 10),
                 Text(
-                  "LKS MART",
+                  "Sign In",
                   style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 28,
                     fontWeight: FontWeight.bold,
+                    fontSize: 30
                   ),
                 ),
+                SizedBox(height: 5),
+                Text(
+                  "Enter Your ID and password to sign in!
+                )
               ],
-            ),
-          ),
+            )
+          ),  
         ],
       ),
     );
